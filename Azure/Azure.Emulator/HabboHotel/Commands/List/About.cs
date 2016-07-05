0e8549c83fe5c92122b8f769e849ce45c2bc2ca2
Franco Sanllehi <fsanllehi@gamerlive.cl>
2016-07-05 00:10:09 -0400

    Editado About.cs

--- Azure/Azure.Emulator/HabboHotel/Commands/List/About.cs
+++ Azure/Azure.Emulator/HabboHotel/Commands/List/About.cs
@@ -17,7 +17,7 @@ namespace Azure.HabboHotel.Commands.List
         public About()
         {
             MinRank = 1;
-            Description = "Shows information about the server.";
+            Description = "Muesta informacion del servidor.";
             Usage = ":about";
             MinParams = 0;
         }
@@ -30,23 +30,33 @@ namespace Azure.HabboHotel.Commands.List
             message.AppendString("Azure");
             message.AppendInteger(4);
             message.AppendString("title");
-            // Respect Azure Emulator and don't remove the developers credits!
-            message.AppendString("¡GamerLive.cL Fixed!");
-            // Respect Azure Emulator and don't remove the developers credits!
+            message.AppendString("Azure Emulator (V0.1)"); //Titulo
             message.AppendString("message");
             var info = new StringBuilder();
-            // Respect Azure Emulator and don't remove the developers credits!
-            info.Append("<h5><b>Emulador editado por Fanco Sanllehi [Privado]</b><h5></br></br>");
-            // Respect Azure Emulator and don't remove the developers credits!
+            info.Append("<h5><b>Azure Emulator Editado por DmACK [Privado]</b><h5></br></br>");
             info.Append("<br />");
             info.Append("<br />");
-            // Respect Azure Emulator and don't remove the developers credits!
-            info.AppendFormat("<b>Creditos</b> <br />-Editores Azure Emulator/>-Sulake /> <br /> ");
-            // Respect Azure Emulator and don't remove the fixers credits!
+            /*
+             * No sacar Creditos de los editores de azure emulator
+             */
+            info.AppendFormat("<b>Creditos:</b>");
+            info.AppendFormat("-Editores Azure Emulator");
+            info.AppendFormat("-Sulake");
+            info.AppendFormat("<br />");
+            /*
+             * Nombres de desarrolladores
+             */
+            info.AppendFormat("<b>Developers</b>");
+            info.AppendFormat("-DmACK (Franco Sanllehi)")
+            info.AppendFormat("<br />")
+            info.Append("<br />");
+            info.Append("<br />");
+            info.Append("<br />");
+            /*
+             * Estadisticas del Hotel
+             */
             info.AppendFormat("<b>[Estadisticas del hotel]</b> <br />");
             var userCount = Azure.GetGame().GetClientManager().Clients.Count;
-            // Respect Azure Emulator and don't remove the developers credits!
-            // Respect Azure Emulator and don't remove the developers credits!
             var roomsCount = Azure.GetGame().GetRoomManager().LoadedRooms.Count;
             info.AppendFormat("<b>Usuarios:</b> {0} en {1}{2}.<br /><br /><br />", userCount, roomsCount,
                 (roomsCount == 1) ? " Sala" : " Salas");
@@ -54,7 +64,7 @@ namespace Azure.HabboHotel.Commands.List
             message.AppendString("linkUrl");
             message.AppendString("event:");
             message.AppendString("linkTitle");
-            message.AppendString("ok");
+            message.AppendString("Cerrar"); // Boton de cerrar
             client.SendMessage(message);
 
             return true;
